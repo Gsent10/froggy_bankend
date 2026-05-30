44f7d06b49eb4031594083120d33a7f7f5ddc87f
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-05-30 10:07:35 +0100

    Create ResetPasswordRequest.php

---
+++ app/Http/Requests/Customer/ResetPasswordRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Customer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'password.confirmed' => 'Password confirmation does not match.',
+        ];
+    }
+}
